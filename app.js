84612a4141d77666ce853cf546360a90ef1cc5bc
komodokodomo <komodokodomo@gmail.com>
2019-06-17 15:33:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -132,8 +132,8 @@ function draw(){
   noStroke();
   fill(245);
   textSize(round(w/15));
-  textAlign(CENTER);
-  text("Scan the QR code on the QR login page", w/8, h/2+(displayWidth*1/3)*1.3, w-w/8, h/2+(displayWidth*1/3)*1.5);
+  textAlign(CENTER,CENTER);
+  text("Scan the QR code on the QR login page", displayWidth/8, h/2+(displayWidth*1/3)*1.3, displayWidth-displayWidth/8, h/2+(displayWidth*1/3)*1.5);
   
   fill(245);
   noStroke();
@@ -141,7 +141,7 @@ function draw(){
 
   fill(100);
   textSize(round(w/15));
-  textAlign(CENTER);
+  textAlign(CENTER,CENTER);
   text("Scan QR code", w/2, h/20);
 	}
 
