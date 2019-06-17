4982e7e74b6d6455cb33b9b76964f5a0a1824c53
komodokodomo <komodokodomo@gmail.com>
2019-06-17 16:44:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ function mouseClicked() {
   }
   }
   else if(mode==3){
-    if(mouseX<w/5 && mouseY<w/5){mode=2;resizeCanvas(width, width*mainpage.height/mainpage.width);}
+    if(mouseX<w/5 && mouseY<w/5){mode=2;resizeCanvas(width, width*mainpage.height/mainpage.width);scanner.stop();}
   }
   console.log(document.location.href);
 
