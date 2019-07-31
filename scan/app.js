da334e442b3383dcff08163fe3a21e4eb856617d
zhideloh <lohzhide@gmail.com>
2019-07-31 22:29:14 +0800

    Update app.js

--- scan/app.js
+++ scan/app.js
@@ -69,7 +69,7 @@ function setup(){
   button.mousePressed(startSketch);
 
   Quiet.init({
-    profilesPrefix: "",
+    profilesPrefix: "/scan/",
     memoryInitializerPrefix: "/",
     libfecPrefix: "/"
 });
