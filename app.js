12a9c694227ca661dc7f7b2384eb7dd76d04ba83
zhideloh <lohzhide@gmail.com>
2019-08-13 11:34:40 +0800

    worklurk

---
+++ app.js
@@ -0,0 +1,66 @@
+let logo;
+let region;
+
+var w;
+var h;
+var mode = 0;
+var code = "";
+
+var mic;
+
+var socket;
+
+
+function startCon(){
+	socket = io('cotf.cf', {
+});
+	socket.on('connect', function() {
+		socket.emit('hello');
+		console.log("connected");		 
+	});
+}
+
+function setup(){
+  w = window.innerWidth;
+  h = window.innerHeight;
+	createCanvas(w,h);
+  region = createImage(displayWidth-displayWidth*2/3,displayWidth-displayWidth*2/3);
+  logo = loadImage('assets/qr.png');
+  
+  mic = new p5.AudioIn()
+  mic.start();
+  mic.amp(1.0);
+
+
+  userStartAudio(mic).then(function() {;
+  });
+
+  startCon();
+}
+
+
+
+
+
+
+function draw(){
+  if(mode==0){
+    background(245);
+    imageMode(CENTER);
+    image(logo,w/2,h/2,w*44/100,w*44/(100*logo.width)*logo.height);
+    }
+  else if(mode == 1){
+    background(245);
+    textAlign(CENTER,CENTER);
+    textSize(32);
+    text(code,w/2,h/2);
+  }
+
+}
+
+function mouseClicked() {
+  socket.emit('hello');
+}
+
+
+
