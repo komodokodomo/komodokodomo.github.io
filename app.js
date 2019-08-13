92ec9973dc29ba49c83f10e8669f9c7198b38510
zhideloh <lohzhide@gmail.com>
2019-08-13 16:55:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ function draw(){
     }
   else if(mode == 1){
     var spectrum = fft.analyze();
-    console.log(fft.getEnergy(17897));
+    var energy = fft.getEnergy(17897);
+    console.log(energy);
     background(245);
     textAlign(CENTER,CENTER);
     textSize(32);
