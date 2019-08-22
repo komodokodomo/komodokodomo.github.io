fc630cca1c88aaba13d175299b784da62e8c2d33
zhideloh <lohzhide@gmail.com>
2019-08-22 10:58:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function setup(){
   createCanvas(w,h);
 
   console.log(jsonFile[0].question);
-  console.log(jsonFile[0].0);
+  // console.log(jsonFile[0].0);
 
   if(w > h){refDimensions = h;}
   else{refDimensions = w;}
