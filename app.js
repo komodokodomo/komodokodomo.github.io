00e61c9ce1266c463333a93d39246d296f54ab7f
zhideloh <lohzhide@gmail.com>
2019-08-22 09:28:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ function preload() {
 
 function loadImages()
 {
+  console.log(jsonFile.length);
   for(var i =0; i<jsonFile.length; i++)
   {
     images[i] = loadImage(jsonFile[i].link);
@@ -317,7 +318,7 @@ function checkRegionChange()
       {
         buttonOpt[i].size(w/2,h/6);
         buttonOpt[i].position((i%2)*w/2,floor(i/2)*h/6 + 2*h/3);
-        buttonOpt[i].html(jsonFile[beaconChosen].i);
+        buttonOpt[i].html(jsonFile[beaconChosen].(i.toString()));
         buttonOpt[i].show();
       }
       questionText.html(jsonFile[beaconChosen].question);
