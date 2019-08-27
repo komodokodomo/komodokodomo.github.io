6a1755922d00dbbb4dbc490e7bdcb72370abd656
zhideloh <lohzhide@gmail.com>
2019-08-27 21:50:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ function scanBeacon()
       {
         lastPingPeakCounter[i] = 0;
         lastPingEnergy[i] = 0;
-        beaconChosen = 99;
+        if(i==beaconChosen){beaconChosen = 99;}
         // console.log("band:" + i +", last ping: " + lastPingPeakPeriod[i]+", counter: " + lastPingPeakCounter[i]);
       }
     }
