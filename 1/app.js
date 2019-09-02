aea730fc6a6f5b91a4a7c976b00be1b8671668bd
zhideloh <lohzhide@gmail.com>
2019-09-02 15:34:08 +0800

    update

---
+++ 1/app.js
@@ -0,0 +1,37 @@
+var w,h;
+
+
+
+
+
+function setup() {
+  w = window.innerWidth;
+  h = window.innerHeight;
+  createCanvas(w, h);
+  background(240);
+  strokeWeight(4);
+  line(w/3,0,w/3,h);
+  line(w/3,h/3,w,h/3);
+  textMode(CENTER,CENTER)
+  text("Users attempting same task",w/6,h/9);
+}
+
+function draw() {
+  
+}
+
+function mouseClicked() 
+{
+}
+
+function windowResized()
+{
+  w = window.innerWidth;
+  h = window.innerHeight;
+  resizeCanvas(w, h);
+  background(240);
+  strokeWeight(4);
+  line(w/3,0,w/3,h);
+  line(w/3,h/3,w,h/3);
+  text("Users attempting same task",w/6,h/9);
+}
