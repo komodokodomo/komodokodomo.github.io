227e972cf92ee687a1d28797cd41665a484e3b35
zhideloh <lohzhide@gmail.com>
2019-09-02 15:46:06 +0800

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -17,8 +17,8 @@ function setup() {
 
   noStroke();
   textAlign(CENTER,CENTER);
-  textSize(w/50);
-  text("Users attempting same task",w/6,h/9);
+  textSize(w/80);
+  text("Users attempting same task",w/8,h/10);
 }
 
 function draw() {
@@ -42,6 +42,6 @@ function windowResized()
   line(w/4,3*h/5,w,3*h/5);
  
   noStroke();
-  textSize(w/50);
-  text("Users attempting same task",w/6,h/9);
+  textSize(w/80);
+  text("Users attempting same task",w/8,h/10);
 }
