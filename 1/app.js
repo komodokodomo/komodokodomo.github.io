c909d1daf1d75e2268d2cef72a14eabf1b905058
zhideloh <lohzhide@gmail.com>
2019-09-02 15:44:28 +0800

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -40,7 +40,8 @@ function windowResized()
   strokeWeight(4);
   line(w/4,0,w/4,h);
   line(w/4,3*h/5,w,3*h/5);
-
+ 
   noStroke();
+  textSize(w/50);
   text("Users attempting same task",w/6,h/9);
 }
