119d1fb919a686e45564366c13209aa057660ed8
zhideloh <lohzhide@gmail.com>
2019-09-02 15:41:12 +0800

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -9,12 +9,15 @@ function setup() {
   h = window.innerHeight;
   createCanvas(w, h);
   background(240);
+  
   stroke(150);
   strokeWeight(4);
   line(w/3,0,w/3,h);
   line(w/3,3*h/5,w,3*h/5);
+
+  noStroke();
   textAlign(CENTER,CENTER);
-  textSize(w/10);
+  textSize(w/100);
   text("Users attempting same task",w/6,h/9);
 }
 
@@ -32,8 +35,12 @@ function windowResized()
   h = window.innerHeight;
   resizeCanvas(w, h);
   background(240);
+
+  stroke(150);
   strokeWeight(4);
   line(w/3,0,w/3,h);
   line(w/3,3*h/5,w,3*h/5);
+
+  noStroke();
   text("Users attempting same task",w/6,h/9);
 }
