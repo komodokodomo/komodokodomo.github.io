d37601c803aafd2fe6ab1f7f39186e030615d88d
zhideloh <lohzhide@gmail.com>
2019-09-04 10:51:44 +0800

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -132,7 +132,7 @@ function setup() {
 
     chatDivNameButtons[2*i+1] = createButton(buttonText[(2*i+1)%2]);
     chatDivNameButtons[2*i+1].size(w/12,h/20);
-    chatDivNameButtons[2*i+1].position(w/8,(i+1)*h/10 + i*h/20);
+    chatDivNameButtons[2*i+1].position(w/8,(i+1)*h/10 + (i+1)*h/20);
     
   }
 
