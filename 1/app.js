3ce10d8aba11d24399530d7f8f302e30c0dc8cd0
zhideloh <lohzhide@gmail.com>
2019-09-05 10:19:56 +0800

    Update app.js

--- 1/app.js
+++ 1/app.js
@@ -15,7 +15,7 @@ var chatDiv,chatDivText;
 
 var submitButton;
 var inputBox;
-var inputBoxPlaceholder = ["hello","yes","nope"];
+var inputBoxPlaceholder = ["Knight / Knave / Joker","A / B / C / D / E","______ minutes"];
 
 var chatDivName = [];
 var chatDivNameText = [];
