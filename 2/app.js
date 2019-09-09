2d8b050a83a909cbaf6b13691a4b0ddb6377b2f6
zhideloh <lohzhide@gmail.com>
2019-09-09 15:21:25 +0800

    Update app.js

--- 2/app.js
+++ 2/app.js
@@ -192,21 +192,8 @@ function setup() {
 
 }
 
-function shuffle(arra1) {
-  var ctr = arra1.length, temp, index;
-
-// While there are elements in the array
-  while (ctr > 0) {
-// Pick a random index
-      index = Math.floor(Math.random() * ctr);
-// Decrease ctr by 1
-      ctr--;
-// And swap the last element with it
-      temp = arra1[ctr];
-      arra1[ctr] = arra1[index];
-      arra1[index] = temp;
-  }
-  return arra1;
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
 
 function typeEvent(){
