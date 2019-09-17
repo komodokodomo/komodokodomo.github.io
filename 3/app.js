056ba4a9c3102064280fcfbf281bf6e69c1b1238
zhideloh <lohzhide@gmail.com>
2019-09-17 10:17:55 +0800

    Update app.js

--- 3/app.js
+++ 3/app.js
@@ -196,7 +196,7 @@ function setup() {
     chatDivNameButtons[2*i].style("right","4%");
     chatDivNameButtons[2*i].style("top","50%");
     chatDivNameButtons[2*i].style("height","80%");
-    chatDivNameButtons[2*i].style("width","43%");
+    chatDivNameButtons[2*i].style("width","35%");
     chatDivNameButtons[2*i].style("transform","translate(0%, -50%)");
     chatDivNameButtons[2*i].mousePressed(chatEnding);
 
