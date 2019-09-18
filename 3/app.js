5352a6cfe5a708d65dac0272aa2e4d88e7040192
zhideloh <lohzhide@gmail.com>
2019-09-18 13:38:51 +0800

    Update app.js

--- 3/app.js
+++ 3/app.js
@@ -40,11 +40,10 @@ var timeStamp = 0;
 var images = []
 var imageDivLinks = 
 [
-"https://drive.google.com/uc?export=view&id=1USRfKs-oXwrBoYj6hN8vHPrmrczWRNjW",
+"https://drive.google.com/uc?export=view&id=1Gx4XIIpuJTP3YeZ5Gy2ilF5NyY0pYkqP",
 "https://drive.google.com/uc?export=view&id=1SGm3QJiZBBRM2s1J7JQJ0wyqmPTHUNEf",
 "https://drive.google.com/uc?export=view&id=1RrxfFXtnvam5HsZSyoMkcPcjp0bxCes_"
 ];
-
 // https://drive.google.com/uc?export=view&id=1USRfKs-oXwrBoYj6hN8vHPrmrczWRNjW
 // https://drive.google.com/uc?export=view&id=1SGm3QJiZBBRM2s1J7JQJ0wyqmPTHUNEf
 // https://drive.google.com/uc?export=view&id=1RrxfFXtnvam5HsZSyoMkcPcjp0bxCes_
