867fedce0372707220a3d175e9a5dd92160a276a
zhideloh <lohzhide@gmail.com>
2019-09-23 11:42:58 +0800

    Update app.js

--- phone/app.js
+++ phone/app.js
@@ -4,14 +4,21 @@ var beaconNumber;
 var timer = 0;
 var on = false;
 
+var beacon =[17429,17778,18141,18476,18824,19185,19560,19950,20356];
+
 function setup() {
   textAlign(CENTER);
 
   sel = createSelect();
   sel.position(10, 10);
-  sel.option('0');
-  sel.option('1');
-  sel.option('2');
+  sel.option('channel 0');
+  sel.option('channel 1');
+  sel.option('channel 2');
+  sel.option('channel 3');
+  sel.option('channel 4');
+  sel.option('channel 5');
+  sel.option('channel 6');
+  sel.option('channel 7');
   sel.changed(mySelectEvent);
 
   osc = new p5.Oscillator();
@@ -27,31 +34,19 @@ function setup() {
 
 function draw() {
   background(220);
-  if(millis()-timer>40 && !on){osc.amp(1.0);on = true;timer=millis();}
-  if(millis()-timer>160 && on){osc.amp(0.0);on = false;timer=millis();}
-
-  // text('click to play', width/2, height/2);
+  if(millis()-timer>40 && !on){osc.amp(1.0,5);on = true;timer=millis();}
+  if(millis()-timer>160 && on){osc.amp(0.0,5);on = false;timer=millis();}
 }
 
-// function mouseClicked() {
-//   if (mouseX > 0 && mouseX < width && mouseY < height && mouseY > 0) {
-//     if (!playing) {
-//       // ramp amplitude to 0.5 over 0.05 seconds
-//       osc.amp(0.5, 0.05);
-//       playing = true;
-//       backgroundColor = color(0,255,255);
-//     } else {
-//       // ramp amplitude to 0 over 0.5 seconds
-//       osc.amp(0, 0.5);
-//       playing = false;
-//       backgroundColor = color(255,0,255);
-//     }
-//   }
-// }
 
 function mySelectEvent() {
   var item = sel.value();
-  if(item == "0"){osc.freq(15000);}
-  else if(item == "1"){osc.freq(18824);}
-  else if(item == "2"){osc.freq(19185);}
+  if(item == "channel 0"){osc.freq(beacon[0]);}
+  else if(item == "channel 1"){osc.freq(beacon[1]);}
+  else if(item == "channel 2"){osc.freq(beacon[2]);}
+  else if(item == "channel 3"){osc.freq(beacon[3]);}
+  else if(item == "channel 4"){osc.freq(beacon[4]);}
+  else if(item == "channel 5"){osc.freq(beacon[5]);}
+  else if(item == "channel 6"){osc.freq(beacon[6]);}
+  else if(item == "channel 7"){osc.freq(beacon[7]);}
 }
