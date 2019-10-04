1daceeee10e42ac6d639878551fc2b25b0d5c3e4
zhideloh <lohzhide@gmail.com>
2019-10-04 10:24:46 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -116,8 +116,8 @@ function setup(){
   w = window.innerWidth;                                                    
   h = window.innerHeight;
   canvas = createCanvas(w,h);
-  map = mappa.tileMap(0,0,4); 
-  map.overlay(canvas);
+  // map = mappa.tileMap(0,0,4); 
+  // map.overlay(canvas);
 
   for(var i =0; i<Object.keys(jsonFile).length; i++)
   {
