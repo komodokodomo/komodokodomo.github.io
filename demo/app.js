65dff8a5593e04a5eb262e0bff73bd192fbc0bb6
zhideloh <lohzhide@gmail.com>
2019-10-04 16:12:35 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -125,6 +125,9 @@ function setup(){
   mapDiv.id("map");
 
   map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
   // map = mappa.tileMap(0,0,4); 
   // map.overlay(canvas);
 
