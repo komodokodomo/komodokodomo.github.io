c2b4d72b70dc66f3105fef8a46c352459bfaaee8
zhideloh <lohzhide@gmail.com>
2019-10-06 16:04:52 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -136,7 +136,7 @@ function doThisOnLocation(position){
   console.log(position.accuracy);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-  myMap.map.removeControl(myMap.map.zoomControl);
+  document.getElementsByClassName("leaflet-top leaflet-left").hide();
   // myMap.map.flyTo([-33.448890, -70.669265], 9);
 
 
