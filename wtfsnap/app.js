8811e0d2695a43e7ac814fb64bbda07fee61c18b
zhideloh <lohzhide@gmail.com>
2019-10-17 16:47:30 +0800

    Update app.js

--- wtfsnap/app.js
+++ wtfsnap/app.js
@@ -7,7 +7,11 @@ var swipeDisplacement = 0;
 var swipeTimer = 0;
 
 var constraints = {
-  video: { facingMode: { exact: "environment" } },
+  video: { 
+  facingMode: { exact: "environment" },
+  width: { min: 1080 },
+  height: { min: 1920 } 
+},
   audio: false
 
 };
