4c324a96c6b52e6ff51f9b7944bf7c2b47f20974
zhideloh <lohzhide@gmail.com>
2019-10-20 20:36:39 +0800

    Update app.js

--- wtfsnap/app.js
+++ wtfsnap/app.js
@@ -18,8 +18,8 @@ var constraints = {
 var itemsText = [];
 
 let video;
-// let yolo = ml5.YOLO(modelReady);
-let yolo;
+let yolo = ml5.YOLO(modelReady);
+// let yolo;
 
 let objects = [];
 var starting = false;
@@ -71,14 +71,14 @@ video2.srcObject = stream;
   // yolo = ml5.YOLO(sampleImage, 
   //   // { filterBoxesThreshold: 0.01, IOUThreshold: 0.3, classProbThreshold: 0.25 },
   //   detect);
-  yolo = ml5.YOLO(video, detect,modelReady);
+  // yolo = ml5.YOLO(video, detect,modelReady);
   prevX = mouseX;
 }
 
 function modelReady() {
   console.log("model Ready!")
   status = true;
-  // yolo.detect(video2, detect);
+  yolo.detect(video2, detect);
 }
 
 
