96b00c9e8897fd9e8791b2e33839ac964b1a41c6
zhideloh <lohzhide@gmail.com>
2019-10-26 15:28:12 +0800

    Update app.js

--- wtfsnap/app.js
+++ wtfsnap/app.js
@@ -124,7 +124,8 @@ if(w>h){
 }
 else{
 // image(video, w/2, h/2,w,h);
-image(video, w/2, h/2, w, w*h/videoWidth);
+image(video, w/2, h/2, h*videoWidth/videoHeight, h);
+// image(video, w/2, h/2, w, w*h/videoWidth);
 }
 
 
