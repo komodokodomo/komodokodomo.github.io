27e6dd086832d93871fa526f03d3af16733124f3
zhideloh <lohzhide@gmail.com>
2019-11-01 15:13:33 +0800

    Update app.js

--- wtfsnap/app.js
+++ wtfsnap/app.js
@@ -99,6 +99,7 @@ function setup() {
     // subjects[i].mouseClicked(function(){subjects[i].addClass('active');});
     document.getElementById("li"+i.toString()).onclick = function(){
       console.log("you clicked: " + i.toString());
+      document.getElementsByClassName("active").classList.remove("active");
       document.getElementById("li"+i.toString()).classList.add("active");
     };
     // subjects[i].style("display","inline");
