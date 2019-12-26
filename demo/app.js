62b24fb599531658d1c9946548ae0a2de6a1213e
zhideloh <lohzhide@gmail.com>
2019-12-26 16:49:11 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -167,9 +167,9 @@ function doThisOnLocation(position){
 
   var profileIcon = L.icon({
     iconUrl: 'profile.png',
-    iconSize:     [96, 96], // size of the icon
-    iconAnchor:   [48, 48], // point of the icon which will correspond to marker's location
-    popupAnchor:  [48, 48] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [48, 48], // size of the icon
+    iconAnchor:   [24, 24], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, 24] // point from which the popup should open relative to the iconAnchor
   });
   // L.CircleMarker
   currentPosition = L.marker([lat, lng],{icon: profileIcon})
