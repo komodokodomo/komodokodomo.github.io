d7d13fedd510187c73f10643407ea6dd8bfe0261
zhideloh <lohzhide@gmail.com>
2019-12-27 18:17:11 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -220,7 +220,7 @@ function outsideTheFence(position){
     for(var j=0; j<geoData.features[i].geometry.coordinates[0].length; j++){
     fence[i].push({lat:geoData.features[i].geometry.coordinates[0][j][0],lon:geoData.features[i].geometry.coordinates[0][j][1]});
     }
-    fenceAlert[i] = new geoFencePolygon(fence[i], function(){ console.log("i am in area " + i.toString()); }, function(){ console.log("i am out of the area " + i.toString()); }, 'mi')
+    fenceAlert[i] = new geoFencePolygon(fence[i], function(){ console.log("i am in area " + i.toString()); }, function(){ console.log("i am out of area " + i.toString()); }, 'km')
     // console.log(i);
   }
 
