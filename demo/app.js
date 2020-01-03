2d34e95a446b169fe984691389c3d85c93ecde6b
zhideloh <lohzhide@gmail.com>
2020-01-03 09:31:14 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -197,9 +197,9 @@ function outsideTheFence(position){
   // L.CircleMarker
   currentPosition = L.marker([lat, lng],{icon: profileIcon})
   .bindPopup("<h1>Profile</h1><p>noob explorer</p><br><p>last trip: 22 days ago</p>",{width: "auto"}).on("popupopen", () => {
-      this.preventDefault();
-      this.update()	;
-      console.log(this);
+      // this.preventDefault();
+      // this.update()	;
+      console.log("hellooo");
     });
     currentPosition.addTo(myMap.map);
   
