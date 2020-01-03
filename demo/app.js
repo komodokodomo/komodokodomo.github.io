5926db8816bdad7abe101d9ff4b5cd81e4dd0021
zhideloh <lohzhide@gmail.com>
2020-01-03 14:28:57 +0800

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -243,6 +243,7 @@ function outsideTheFence(position){
         alert("i am in area " + i.toString());
         infoDiv.show(); 
         infoDiv.style("z-index","5");
+        inforDiv.html("<h2>Sandcrawler 0/2 journeys explored</h2><img src=\"https://upload.wikimedia.org/wikipedia/en/a/aa/Star_Wars_Sandcrawler.png\" class = \"ppic\"><span>3.2 </span><span class=\"fas fa-star\"></span><span class=\"fas fa-star\"></span><span class=\"fas fa-star\"></span><span class=\"far fa-star\"></span><span class=\"far fa-star\"></span><p>Hi there, this is where the people who made this prototype work!</p>");
         fenceAlertTrigger[i] = true;
       }
       },
