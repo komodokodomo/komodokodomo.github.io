6132bf55ebe2a595a0a71530e4771270d6be980a
zhideloh <lohzhide@gmail.com>
2020-01-16 15:13:37 +0800

    Update app.js

--- spice_tf/app.js
+++ spice_tf/app.js
@@ -22,7 +22,7 @@ function videoLoaded(){
 }
 
 async function loadModel() {
-  const modelURL = 'https://worklurk.cf/spice_tf/web_model/model.json';
+  const modelURL = 'spice_tf/web_model/model.json';
   this.model = await tf.loadGraphModel(modelURL);
   const result = await this.model.executeAsync(tf.zeros([1, 300, 300, 3]));
   await Promise.all(result.map(t => t.data()));
