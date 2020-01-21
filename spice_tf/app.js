d7f34ff7801810cd9313091eb752efe7bcfe40d6
zhideloh <lohzhide@gmail.com>
2020-01-21 10:16:52 +0800

    update

--- spice_tf/app.js
+++ spice_tf/app.js
@@ -396,34 +396,60 @@ loginWrapperInputForgot.style("padding","1rem 0rem");
   video = createCapture(constraints);
   video.size(videoWidth, videoHeight);
   video.hide();
-  var test = document.getElementById("canvas");
+  // var test = document.getElementById("canvas");
+
+  const img = document.getElementById('canvas'); 
+  objectDetector.load('model_web') 
+  .then(model => 
+    setInterval(function()
+  {
+    
+    model.detect(img).then(predictions => { 
+      if(!hideButton){
+      console.log(predictions) 
+      objects = [];
+      if(predictions.length > 0){
+      counter++;
+      if(counter>2){counter=2;}
+      // for (let i = 0; i < predictions.length; i++) {
+        objects[0]=predictions[0];
+      // }
+    }
+    else{
+      counter = 0;
+      button.hide();
+    }
+    }
+    })
+
+  },250))
 
 
-  cocoSsd.load({ base: 'mobilenet_v2',modelUrl: "https://cotf.cf/model" }).then(model => {
-    console.log("model loaded!");
-    status = true;
+  // cocoSsd.load({ base: 'mobilenet_v2',modelUrl: "https://cotf.cf/model" }).then(model => {
+  //   console.log("model loaded!");
+  //   status = true;
 
-    setInterval(function() {
+  //   setInterval(function() {
 
-      if(!hideButton) {
-        model.detect(test,maxBoxes).then(predictions => {
-          console.log(predictions);
-          objects = [];
-          if(predictions.length > 0){
-          counter++;
-          if(counter>2){counter=2;}
-          for (let i = 0; i < predictions.length; i++) {
-            objects[i]=predictions[i];
-          }
-        }
-        else{
-          counter = 0;
-          button.hide();
-        }
-        });
-      }
-    }, 250);
-  });
+  //     if(!hideButton) {
+  //       model.detect(test,maxBoxes).then(predictions => {
+  //         console.log(predictions);
+  //         objects = [];
+  //         if(predictions.length > 0){
+  //         counter++;
+  //         if(counter>2){counter=2;}
+  //         for (let i = 0; i < predictions.length; i++) {
+  //           objects[i]=predictions[i];
+  //         }
+  //       }
+  //       else{
+  //         counter = 0;
+  //         button.hide();
+  //       }
+  //       });
+  //     }
+  //   }, 250);
+  // });
 
 
   // cocoSsd.load().then(model => {
