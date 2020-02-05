ac00611ae71346f3d8c3bd12f48cb167d3133431
zhideloh <lohzhide@gmail.com>
2020-02-05 12:04:47 +0800

    update

--- spice_tf/app.js
+++ spice_tf/app.js
@@ -269,6 +269,7 @@ async function setup() {
   chatboxTitle.style("font-weight","bold");
   chatboxTitle.style("position","absolute");
   chatboxTitle.style("left","30px");
+  chatboxTitle.style("inline-size","fit-content");
 
   chatboxContent = createP("Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.");
   chatboxContent.id("chatbox-content");
