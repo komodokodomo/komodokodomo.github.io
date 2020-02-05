37b340f763f146f820874d76738b86ea82417f22
zhideloh <lohzhide@gmail.com>
2020-02-05 09:47:17 +0800

    Update app.js

--- spice_tf/app.js
+++ spice_tf/app.js
@@ -180,7 +180,7 @@ async function setup() {
   chatboxClose = createDiv("X");
   chatboxClose.style("z-index","6");
   // chatboxClose.style("position","absolute");
-  chatboxClose.position(chatbox.position().x,chatbox.position().y);
+  chatboxClose.position(w/2+canvas.width/2-canvasPadding/2,h/10 + canvasPadding);
   console.log(chatbox.position());
   // chatboxClose.hide();
 
