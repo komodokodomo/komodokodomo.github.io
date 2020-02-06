7bd2d961269107830cadb7848437cffbd9a471b5
zhideloh <lohzhide@gmail.com>
2020-02-06 15:09:35 +0800

    Update app.js

--- scgs/app.js
+++ scgs/app.js
@@ -237,7 +237,7 @@ async function setup() {
   lensContainer.hide();
 
   for(let i = 0; i<3; i++){
-    faces[i] = createImg("https://cors-anywhere.herokuapp.com/"+jsonData[i].Image, i.toString());
+    // faces[i] = createImg("https://cors-anywhere.herokuapp.com/"+jsonData[i].Image, i.toString());
     faces[i] = createImg("300ppi/faces"+((i+1)*2).toString()+".png", i.toString());
     faces[i].parent(lensContainer);
     faces[i].style("position","absolute");
