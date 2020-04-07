33ceefce01a0b2b6e8c893359c53c4cb274f5e1f
zhideloh <lohzhide@gmail.com>
2020-04-07 13:54:53 +0800

    update

--- tcss/js/app.js
+++ tcss/js/app.js
@@ -4,6 +4,8 @@ var DOM_EL = {
     loginTitle: null,
     loginInstruction: null,
     loginSliderContainer: null,
+    loginSliderArrowLeft: null,
+    loginSliderArrowRight: null,
     images: [],
     loginInput: null,
     loginButton: null
@@ -72,6 +74,10 @@ function setup(){
     DOM_EL.loginSliderContainer.id("slider");
     DOM_EL.loginSliderContainer.parent(DOM_EL.loginContainer);
 
+    DOM_EL.loginSliderArrowLeft = createImage('img/left.png');
+    DOM_EL.loginSliderArrowLeft.id("login-slider-arrow-left");
+    DOM_EL.loginSliderArrowLeft.parent(DOM_EL.loginContainer);
+
     for( let i = 0; i < (APP_STATE.numSprites)*4; i++ ){
         DOM_EL.images[i] = createImg("img/" + i.toString() + ".png");
         DOM_EL.images[i].hide();
@@ -82,6 +88,10 @@ function setup(){
     }
 
     DOM_EL.images[0].show();
+
+    DOM_EL.loginSliderArrowRight = createImage('img/right.png');
+    DOM_EL.loginSliderArrowRight.id("login-slider-arrow-right");
+    DOM_EL.loginSliderArrowRight.parent(DOM_EL.loginContainer);
 
     DOM_EL.loginInput = createInput();
     DOM_EL.loginInput.id("login-input");
