9ed83cf96f02dee8a99b69d0b6d7f0cc25c6517f
zhideloh <lohzhide@gmail.com>
2020-04-07 15:47:20 +0800

    Update app.js

--- tcss/js/app.js
+++ tcss/js/app.js
@@ -90,7 +90,7 @@ function setup(){
         DOM_EL.images[i] = createImg("img/" + i.toString() + ".png");
         DOM_EL.images[i].hide();
         if( i % 4 === 0 ){
-            DOM_EL.images[i].parent(DOM_EL.loginSliderContainer);
+            DOM_EL.images[i].parent(DOM_EL.loginSliderImageContainer);
             DOM_EL.images[i].class("slider-image");
         }
     }
