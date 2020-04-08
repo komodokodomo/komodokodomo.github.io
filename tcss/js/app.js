d966303e3dfa683deb3ca84050cf7b2c25269141
zhideloh <lohzhide@gmail.com>
2020-04-08 18:50:18 +0800

    Update app.js

--- tcss/js/app.js
+++ tcss/js/app.js
@@ -140,25 +140,25 @@ function setup(){
         if(ev.type === "swipeleft") {
             DOM_EL.images[APP_STATE.spriteNum*4].addClass("class_left");
             setTimeout(function(){
-                DOM_EL.images[overflow(APP_STATE.spriteNum - 1) * 4].hide();
+                DOM_EL.images[overflow(APP_STATE.spriteNum + 1) * 4].hide();
                 // while (DOM_EL.shiftLeft.length){
                 //     DOM_EL.shiftLeft[0].classList.remove("left");
                 // }
-                DOM_EL.images[overflow(APP_STATE.spriteNum - 1) * 4].removeClass("class_left");
+                DOM_EL.images[overflow(APP_STATE.spriteNum + 1) * 4].removeClass("class_left");
             }
             ,250);
             APP_STATE.spriteNum++;  
             APP_STATE.spriteNum = overflow(APP_STATE.spriteNum);
-            console.log(APP_STATE.spriteNum);
             DOM_EL.images[APP_STATE.spriteNum*4].show();
+
         } else if(ev.type === "swiperight") {
             DOM_EL.images[APP_STATE.spriteNum].addClass("class_right");
             setTimeout(function(){
-                DOM_EL.images[overflow(APP_STATE.spriteNum + 1) * 4].hide();
+                DOM_EL.images[overflow(APP_STATE.spriteNum - 1) * 4].hide();
                 // while (DOM_EL.shiftRight.length){
                 //     DOM_EL.shiftRight[0].classList.remove("right");
                 // }
-                DOM_EL.images[overflow(APP_STATE.spriteNum + 1) * 4].removeClass("class_right");
+                DOM_EL.images[overflow(APP_STATE.spriteNum - 1) * 4].removeClass("class_right");
             }
             ,250);
             APP_STATE.spriteNum--;  
