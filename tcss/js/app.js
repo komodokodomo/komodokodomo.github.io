7002e49de13bc9115ed127e6989e2b234c373878
zhideloh <lohzhide@gmail.com>
2020-04-08 19:40:27 +0800

    Update app.js

--- tcss/js/app.js
+++ tcss/js/app.js
@@ -146,6 +146,7 @@ function setup(){
                 //     DOM_EL.shiftLeft[0].classList.remove("left");
                 // }
                 DOM_EL.images[temp * 4].removeClass("left");
+                console.log("timeout: " + APP_STATE.spriteNum);
             }
             ,300);
             APP_STATE.spriteNum++;  
@@ -162,6 +163,7 @@ function setup(){
                 //     DOM_EL.shiftRight[0].classList.remove("right");
                 // }
                 DOM_EL.images[temp2 * 4].removeClass("right");
+                console.log("timeout: " + APP_STATE.spriteNum);
             }
             ,300);
             APP_STATE.spriteNum--;  
@@ -169,7 +171,7 @@ function setup(){
             DOM_EL.images[APP_STATE.spriteNum*4].show();
         }
         // APP_STATE.spriteNum = overflow(APP_STATE.spriteNum);
-        // console.log(APP_STATE.spriteNum);
+        console.log("current: " + APP_STATE.spriteNum);
     });
 }
 
