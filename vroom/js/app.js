a06e5ae3ac6dc5af489a41a3315856b20fc14e8a
zhideloh <lohzhide@gmail.com>
2020-04-22 11:41:45 +0800

    Update app.js

--- vroom/js/app.js
+++ vroom/js/app.js
@@ -63,7 +63,7 @@ var myStream,streamObtained = false;
 async function register() {
   
     try {
-      myStream = await navigator.mediaDevices.getUserMedia({video: false, audio: true});
+      myStream = await navigator.mediaDevices.getUserMedia({video: false, audio: true, echoCancellation: true, noiseSupression: true});
       console.log("local stream: ");
       console.log(myStream);
       streamObtained = true;
