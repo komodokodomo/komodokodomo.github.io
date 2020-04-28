f4e29b9c576ff1999119b5716912d37e5ea0ff89
zhideloh <lohzhide@gmail.com>
2020-04-28 13:55:36 +0800

    Update app.js

--- trainer/js/app.js
+++ trainer/js/app.js
@@ -152,7 +152,8 @@ function classSubmitEvent(){
     }
 
     if(APP_STATE.addClass = true){
-        DOM_EL.classInput.value = "";
+        // DOM_EL.classInput.value = "";
+        DOM_EL.classInput.elt.reset();
         APP_STATE.addClass = false;
         let x = document.getElementById("class-select");
         let option = document.createElement("option");
