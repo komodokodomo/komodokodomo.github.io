e856170cfbceed03871f1d4c6fd0e433a46e8036
zhideloh <lohzhide@gmail.com>
2020-05-07 15:43:25 +0800

    Update app.js

--- trainer/js/app.js
+++ trainer/js/app.js
@@ -335,7 +335,7 @@ function trainButtonEvent(){
     //run through number of selected classes
     for (let i = 0; i< DOM_EL.classSampleList.length; i++){
         console.log(DOM_EL.classSampleList[i].class());
-        if(DOM_EL.classSampleList[i].class() == "class-selected"){
+        if(DOM_EL.classSampleList[i].class().contain("class-selected")) {
             console.log(DOM_EL.classSampleListLabel[i]);
             for(let j = 0; j<DOM_EL.imageSampleList[i].elt.childElementCount; j++){}
         }
