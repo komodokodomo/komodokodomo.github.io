9819bb02ceb652601d02efbfab0e43c91bc69f90
zhideloh <lohzhide@gmail.com>
2020-05-08 10:44:47 +0800

    Update app.js

--- trainer/js/app.js
+++ trainer/js/app.js
@@ -346,7 +346,7 @@ function imageAdded(){
               console.log('Done Training! Final Loss: ' +  APP_STATE.loss);
               APP_STATE.modelTrained = true;
             //   featureExtractor.uploadModel(modelUploaded,"myModel");
-            classifier.uploadModel();
+            classifier.uploadModel(modelUploaded,"myModel");
             //   classifier = featureExtractor.classification(DOM_EL.capture);
             }
           });
