92bd5469a9314b26cb16e2d791b19ef4e8a7705b
zhideloh <lohzhide@gmail.com>
2020-08-16 20:10:04 +0800

    Update app.js

--- alp/app.js
+++ alp/app.js
@@ -348,12 +348,12 @@ function captureEvidenceEvent(){
       DOM_EL.evidenceHeader.html(APP_STATE.evidenceCounter.toString()+ "/" + APP_STATE.numClasses + " Evidence Collected");
 
       
-      DOM_EL.contentHeader.html("Evidence " +  DOM_EL.evidenceListItemContainer[APP_STATE.evidenceCounter].attribute("index"));
-      DOM_EL.contentClass.html(DOM_EL.evidenceListItemTitle[APP_STATE.evidenceCounter].html());
-      let t = DOM_EL.evidenceListItemTitle[APP_STATE.evidenceCounter].html().replace( / /g , "_" );
+      DOM_EL.contentHeader.html("Evidence " +  DOM_EL.evidenceListItemContainer[APP_STATE.evidenceCounter-1].attribute("index"));
+      DOM_EL.contentClass.html(DOM_EL.evidenceListItemTitle[APP_STATE.evidenceCounter-1].html());
+      let t = DOM_EL.evidenceListItemTitle[APP_STATE.evidenceCounter-1].html().replace( / /g , "_" );
       changeContent(overflow(APP_STATE.lensCounter, APP_STATE.numLens), t);
       let d = document.getElementById("content-image");
-      d.src = DOM_EL.evidenceListItem[APP_STATE.evidenceCounter].elt.childNodes[0].src;
+      d.src = DOM_EL.evidenceListItem[APP_STATE.evidenceCounter-1].elt.childNodes[0].src;
       DOM_EL.contentContainer.style("display","flex");
       setTimeout(function(){
         DOM_EL.contentContainer.removeClass("fade");
