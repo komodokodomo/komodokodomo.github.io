f6a3353d5adca0afa5424ec670319dc7b9462ac4
zhideloh <lohzhide@gmail.com>
2021-01-14 16:05:12 +0800

    mod

--- bubblewand/app.js
+++ bubblewand/app.js
@@ -1,34 +1,46 @@
+var MOVEMENT = {
+    x : null,
+    y : null,
+    z : null,
+    xx : null,
+    yy : null,
+    zz : null,
+    rotateZ : null,
+    rotateY : null,
+    rotateX : null,
+}
+
 function handleMotionEvent(event) {
 
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
+    MOVEMENT.x = event.accelerationIncludingGravity.x;
+    MOVEMENT.y = event.accelerationIncludingGravity.y;
+    MOVEMENT.z = event.accelerationIncludingGravity.z;
 
-    var xx = event.acceleration.x;
-    var yy = event.acceleration.y;
-    var zz = event.acceleration.z;
+    MOVEMENT.xx = event.acceleration.x;
+    MOVEMENT.yy = event.acceleration.y;
+    MOVEMENT.zz = event.acceleration.z;
 
-    var rotateDegrees = event.rotationRate.alpha;
-    var leftToRight = event.rotationRate.gamma;
-    var frontToBack = event.rotationRate.beta;
+    MOVEMENT.rotateZ = event.rotationRate.alpha;
+    MOVEMENT.rotateY = event.rotationRate.gamma;
+    MOVEMENT.rotateX = event.rotationRate.beta;
 
-    console.log(x +", " + y + ", " + z);
-    console.log(xx +", " + yy + ", " + zz);
-    console.log(rotateDegrees +", " + leftToRight + ", " + frontToBack);
+    // console.log(x +", " + y + ", " + z);
+    // console.log(xx +", " + yy + ", " + zz);
+    console.log(MOVEMENT.rotateZ +", " + MOVEMENT.rotateY + ", " + MOVEMENT.rotateX);
 }
 
 window.addEventListener("devicemotion", handleMotionEvent, true);
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        var rotateDegrees = event.alpha;
-        var leftToRight = event.gamma;
-        var frontToBack = event.beta;
-        console.log(event);
+// if (window.DeviceOrientationEvent) {
+//     window.addEventListener("deviceorientation", function(event) {
+//         var rotateDegrees = event.alpha;
+//         var leftToRight = event.gamma;
+//         var frontToBack = event.beta;
+//         console.log(event);
 
-        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-    }, true);
-}
+//         handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+//     }, true);
+// }
 
 var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
     // do something amazing
